1cd4e5022f3151e7287d3c3cbf9dda867e7a61fd
Uyen Nhi Quang <111651301+uyennhiquang@users.noreply.github.com>
2023-03-08 20:02:11 -0500

    Add menu screen

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ let result = ``;
 let playing = false;
 
 // DOM variables
+const uiMenuEl = document.getElementById("ui--menu");
+const uiGameEl = document.getElementById("ui--game");
+
 const choiceBtn = document.querySelectorAll(".choice");
+const startBtn = document.querySelector(".button--start");
 
 const playerChoiceDisplay = document.getElementById("choice--player");
 const computerChoiceDisplay = document.getElementById("choice--cpu");
@@ -33,15 +37,7 @@ const playRound = function (playerSelection, computerSelection) {
 
   playerSelection = playerSelection.toLowerCase();
 
-  if (playerSelection === computerSelection)
-    /* return `${playerSelection.replace(
-      playerSelection[0],
-      playerSelection[0].toUpperCase()
-    )} vs ${computerSelection.replace(
-      computerSelection[0],
-      computerSelection[0].toUpperCase()
-    )}. You Draw!`; */
-    return "draw";
+  if (playerSelection === computerSelection) return "draw";
   else {
     switch (playerSelection) {
       case "rock":
@@ -63,74 +59,63 @@ const playRound = function (playerSelection, computerSelection) {
         return 'Please select among "Rock, Paper, Scissors".';
     }
   }
-  /* return `You ${winning ? "Win" : "Lose"}! ${
-    winning
-      ? playerSelection.replace(
-          playerSelection[0],
-          playerSelection[0].toUpperCase()
-        )
-      : computerSelection.replace(
-          computerSelection[0],
-          computerSelection[0].toUpperCase()
-        )
-  } beats ${
-    !winning
-      ? playerSelection.replace(
-          playerSelection[0],
-          playerSelection[0].toUpperCase()
-        )
-      : computerSelection.replace(
-          computerSelection[0],
-          computerSelection[0].toUpperCase()
-        )
-  }`; */
   return winning ? "win" : "lose";
 };
 
+startBtn.addEventListener("click", function () {
+  uiMenuEl.classList.add("hidden");
+  uiGameEl.classList.toggle("hidden");
+  playing = true;
+});
+
 for (let i = 0; i < choiceBtn.length; i++) {
   choiceBtn[i].addEventListener("click", function () {
-    playerChoice = choiceBtn[i].getAttribute("data-choice");
-    playerChoiceDisplay.innerText = playerChoice.replace(
-      playerChoice[0],
-      playerChoice[0].toUpperCase()
-    );
+    if (playing) {
+      playerChoice = choiceBtn[i].getAttribute("data-choice");
+      playerChoiceDisplay.innerText = playerChoice.replace(
+        playerChoice[0],
+        playerChoice[0].toUpperCase()
+      );
 
-    computerChoice = getComputerChoice();
-    computerChoiceDisplay.innerText = computerChoice.replace(
-      computerChoice[0],
-      computerChoice[0].toUpperCase()
-    );
+      computerChoice = getComputerChoice();
+      computerChoiceDisplay.innerText = computerChoice.replace(
+        computerChoice[0],
+        computerChoice[0].toUpperCase()
+      );
 
-    roundResult = playRound(playerChoice, computerChoice);
-    console.log(playerChoice, computerChoice);
-    console.log(roundResult);
+      roundResult = playRound(playerChoice, computerChoice);
+      console.log(playerChoice, computerChoice);
+      console.log(roundResult);
 
-    // Work on changing color background of something based on result
-    for (let i = 0; i < choiceBtn.length; i++) {
-      choiceBtn[i].style.color = "black";
-    }
+      // Work on changing color background of something based on result
+      for (let i = 0; i < choiceBtn.length; i++) {
+        choiceBtn[i].style.color = "black";
+      }
 
-    switch (roundResult) {
-      case "win":
-        choiceBtn[i].style.color = "#60b347";
-        ++playerScore;
-        break;
-      case "lose":
-        choiceBtn[i].style.color = "#FA0415";
-        ++computerScore;
-        break;
-    }
-    playerScoreDisplay.innerText = String(playerScore);
-    computerScoreDisplay.innerText = String(computerScore);
+      switch (roundResult) {
+        case "win":
+          choiceBtn[i].style.color = "#60b347";
+          ++playerScore;
+          break;
+        case "lose":
+          choiceBtn[i].style.color = "#FA0415";
+          ++computerScore;
+          break;
+      }
+      playerScoreDisplay.innerText = String(playerScore);
+      computerScoreDisplay.innerText = String(computerScore);
 
-    if (playerScore === 5) {
-      messageResultEl.innerText = "You Win!";
-      messageResultEl.style.color = "#60b347";
-      messageResultEl.style.fontWeight = "bold";
-    } else if (computerScore === 5) {
-      messageResultEl.innerText = "You Lose!";
-      messageResultEl.style.color = "#FA0415";
-      messageResultEl.style.fontWeight = "bold";
+      if (playerScore === 5) {
+        messageResultEl.innerText = "You Win!";
+        messageResultEl.style.color = "#60b347";
+        messageResultEl.style.fontWeight = "bold";
+        playing = false;
+      } else if (computerScore === 5) {
+        messageResultEl.innerText = "You Lose!";
+        messageResultEl.style.color = "#FA0415";
+        messageResultEl.style.fontWeight = "bold";
+        playing = false;
+      }
     }
   });
 }
